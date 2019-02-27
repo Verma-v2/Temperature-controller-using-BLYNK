3172e7862c7d59145a756daaf940a0ae99ff72e2
Gyanendra Verma <gveeiit@gmail.com>
2019-02-28 00:36:06 +0530

    Update temp_controller_blynk.ino

--- temp_controller_blynk.ino
+++ temp_controller_blynk.ino
@@ -35,8 +35,8 @@ int ledPin = 13;
 
 //variable setup
 float dstemp; 
-int mintemp = 23; // minimum temperature
-int maxtemp = 29; // Maximum temperature
+int mintemp = 23; // Default minimum temperature
+int maxtemp = 29; // Default Maximum temperature
 BlynkTimer timer;
 //wifi
 ESP8266 wifi(&EspSerial);
