ca04ae8835f85089c3e42464cb955dd88a39bef9
Gyanendra Verma <gveeiit@gmail.com>
2019-02-27 20:03:23 +0530

    Update temp_controller_blynk.ino

--- temp_controller_blynk.ino
+++ temp_controller_blynk.ino
@@ -1,25 +1,8 @@
-/*@@ -0,0 +1,21 @@
-MIT License
+/*** Copyright [2019] [Gyanendra Verma]
 
-Copyright (c) 2019 Gyanendra Verma
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
 * we are not responsible for any physical damage caused by the user or to the user. Saftey precautions are mandotery while tesing 
 * We recommande user's to test the code in care of mentors.
 * while using ac load please beware, a mistake could cause a big accident.
